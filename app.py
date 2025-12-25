d34da86c9d7d3e38e20e1d7ed6519182ac1c104b
00den4yiz <00den4yiz@gmail.com>
2025-12-25 15:49:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def chat():
     except Exception as e:
         print(e)
         return jsonify({"response": "Cevap alınamadı"})
-
+        
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  # Render otomatik PORT değişkeni verir
     app.run(host="0.0.0.0", port=port)
